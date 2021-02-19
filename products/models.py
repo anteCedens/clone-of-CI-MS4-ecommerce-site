6aa779d62dd5ba8eeeb8000d87f05d1f8f996ca1
anteCedens <jaksa.juric@yahoo.com.hr>
2021-02-19 15:43:16 +0000

    Update fields for 'Product' in models
    - change 'author' to TextField
    - uncomment 'image_url'
    - add 'publisher', 'year', and 'language'

--- products/models.py
+++ products/models.py
@@ -30,9 +30,12 @@ class Product(models.Model):
     """
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.TextField(max_length=254)
-    author = models.CharField(max_length=254)
+    author = models.TextField(max_length=254)
+    publisher = models.CharField(max_length=254, null=True, blank=True)
+    year = models.IntegerField(max_length=4, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    language = models.CharField(max_length=254, null=True, blank=True)
     rating = models.DecimalField(
         max_digits=6, decimal_places=2, null=True, blank=True)
-    # image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
