a88cc2b24e2e09afdb656d7d7e99b765fb9f2c1e
anteCedens <jaksa.juric@yahoo.com.hr>
2021-03-20 18:57:12 +0000

    Add label for image_url in Product & make migrations and migrate

--- products/models.py
+++ products/models.py
@@ -38,7 +38,7 @@ class Product(models.Model):
     language = models.CharField(max_length=254, null=True, blank=True)
     rating = models.DecimalField(
         max_digits=6, decimal_places=2, null=True, blank=True)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image_url = models.URLField('Image URL', max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
