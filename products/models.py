9cc3dce9c91c995c9ea487ea259178eb945d0c69
anteCedens <jaksa.juric@yahoo.com.hr>
2021-03-20 00:19:31 +0000

    Heed suggested error, make migrations and migrate

--- products/models.py
+++ products/models.py
@@ -13,7 +13,7 @@ class Category(models.Model):
         verbose_name_plural = 'Categories'
 
     name = models.CharField(max_length=254)
-    friendly_name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, blank=True, null=True)
 
     def __str__(self):
         return self.friendly_name
@@ -32,7 +32,7 @@ class Product(models.Model):
     Each product will require a name, author, and price,
     but everything else is set as optional
     """
-    sku = models.CharField('SKU', max_length=254, blank=True, unique=True)
+    sku = models.CharField('SKU', max_length=254, blank=True, unique=True, null=True)
     name = models.TextField(max_length=254)
     author = models.TextField(max_length=254)
     publisher = models.ForeignKey(
@@ -40,12 +40,12 @@ class Product(models.Model):
     # publisher = models.CharField(max_length=254, blank=True)
     year = models.PositiveIntegerField(null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    language = models.CharField(max_length=254, blank=True)
+    language = models.CharField(max_length=254, blank=True, null=True)
     rating = models.DecimalField(
         max_digits=6, decimal_places=2, null=True, blank=True)
-    image_url = models.URLField('Image URL', max_length=1024, blank=True)
-    image = models.ImageField(blank=True)
-    friendly_name = models.CharField(max_length=32, blank=True)
+    image_url = models.URLField('Image URL', max_length=1024, blank=True, null=True)
+    image = models.ImageField(blank=True, null=True)
+    friendly_name = models.CharField(max_length=32, blank=True, null=True)
 
     def __str__(self):
         return self.name
@@ -64,21 +64,21 @@ class Author(models.Model):
         null=True, blank=True)
     year_of_death = models.PositiveIntegerField(
         null=True, blank=True)
-    bio = models.TextField(blank=True)
-    external_links = models.URLField(blank=True)
-    image_url = models.URLField('Image URL', max_length=1024, blank=True)
-    image = models.ImageField(blank=True)
+    bio = models.TextField(blank=True, null=True)
+    external_links = models.URLField(blank=True, null=True)
+    image_url = models.URLField('Image URL', max_length=1024, blank=True, null=True)
+    image = models.ImageField(blank=True, null=True)
 
     def __str__(self):
         return self.author_name
 
 
 class Publisher(models.Model):
-    name = models.CharField(max_length=254)
-    description = models.TextField(blank=True)
-    external_links = models.URLField(blank=True)
-    image_url = models.URLField('Image URL', max_length=1024, blank=True)
-    image = models.ImageField(blank=True)
+    name = models.CharField(max_length=254, blank=True)
+    description = models.TextField(blank=True, null=True)
+    external_links = models.URLField(blank=True, null=True)
+    image_url = models.URLField('Image URL', max_length=1024, blank=True, null=True)
+    image = models.ImageField(blank=True, null=True)
 
     def __str__(self):
         return self.name
