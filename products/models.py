073829e7a2ce7c77ad9ce1676f72f73eef391874
anteCedens <jaksa.juric@yahoo.com.hr>
2021-03-22 00:14:50 +0000

    Remove blank=True for Category friendly_name

--- products/models.py
+++ products/models.py
@@ -12,7 +12,7 @@ class Category(models.Model):
     class Meta:
         verbose_name_plural = 'Categories'
     name = models.CharField(max_length=254)
-    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+    friendly_name = models.CharField(max_length=254, null=True)
 
     def __str__(self):
         return self.friendly_name
