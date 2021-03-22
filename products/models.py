b35c6ef8e8c2b489f7becf203988037094010bf2
anteCedens <jaksa.juric@yahoo.com.hr>
2021-03-22 00:20:40 +0000

    Add labels to sku and image_url in Product & migrate

--- products/models.py
+++ products/models.py
@@ -28,14 +28,13 @@ class Product(models.Model):
     Each product will require a name, author, and price,
     but everything else is set as optional
     """
-    sku = models.CharField(max_length=254, null=True, blank=True)
+    sku = models.CharField('SKU', max_length=254, null=True, blank=True)
     name = models.TextField(max_length=254)
     author = models.TextField(max_length=254)
     publisher = models.CharField(max_length=254, null=True, blank=True)
     year = models.IntegerField(null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    language = models.CharField(max_length=254, null=True, blank=True)
     rating = models.DecimalField(
         max_digits=6, decimal_places=2, null=True, blank=True)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image_url = models.URLField('Image URL', max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
